b497b27a8beed8865a09068b9706452bb71d6a02
alen899-my <alenjames899@gmail.com>
2025-10-07 17:07:09 +0530

    data validation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 if __name__=='__main__':
@@ -11,7 +13,15 @@ if __name__=='__main__':
         DataIngestions=DataIngestion(data_ingestionconfig)
         logging.info("initiate the data ingestion")
         data_ing_artifacts=DataIngestions.initiate_data_ingestion()
+        logging.info("data initiation completed")
         print(data_ing_artifacts)
+        data_validation_config=DataValidationConfig(trainingpipeline_confg)
+        data_validation=DataValidation(data_ing_artifacts,data_validation_config)
+        logging.info("initiate daata validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data validation compeleted")
+        print(data_validation_artifact)
+        
     except Exception as e:
            raise NetworkSecurityException(e,sys)
     
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -14,6 +14,8 @@ FILE_NAME:str="phisingData.csv"
 TRAIN_FILE_NAME="train.csv"
 TEST_FILE_NAME="test.csv"
 
+SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
+
 """
 data ingestion constants for training pipeline
 """
@@ -24,3 +26,11 @@ DATA_INGESTION_DIR_NAME:str="data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR="feature_store"
 DATA_INGESTION_INGESTED_DIR="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float=0.2
+
+
+
+DATA_VALIDATION_DIR_NAME:str="data_validation"
+DATA_VALIDATION_VALID_DIR:str="validated"
+DATA_VALIDATION_INVALID_DIR:str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
